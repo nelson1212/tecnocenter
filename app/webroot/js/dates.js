116f8c03bd93af0407199f5eaceae534712d2310
nelson1212 <nelson.hidalgo1@gmail.com>
2011-02-20 22:48:28 -0500

    Otr

---
+++ app/webroot/js/dates.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $("#ReporteFechaInicial").datepicker({ dateFormat:'yy-mm-dd',appendText: '(año-mes-dia)' });
+    $("#ReporteFechaFinal").datepicker({ dateFormat:'yy-mm-dd',appendText: '(año-mes-dia)' });
+  });
